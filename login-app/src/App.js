1c272bd02ee98a3da516b2012ca083787db53ce4
M.V.LOKESH <lokeshchowdary487@gmail.com>
2021-03-04 15:39:59 +0530

    firstcommit

---
+++ login-app/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import "./App.css";
+
+function App() {
+    const adminUser= {
+        email:"admin12@gmail.com",
+        password:"admin123"
+    }
+    const [user,setUser] = useState({name: "",email: ""});
+    <div className= 'App'>
+        <h1>Hello world</h1>
+    </div>
+};
+
+export default App;
